a13d9085bdf29b0b96fc4f7499aeaa9a76f65976
PC201903251115B\Administrator <2920892312@qq.com>
2019-05-31 16:20:44 +0800

    注释

--- woodgame/utils.js
+++ woodgame/utils.js
@@ -1,9 +1,14 @@
 const log = console.log.bind(console)
 
-const e = selector => document.querySelector(selector)
+// 可以对 log 进行一些骚操作
+// 比如让 log 实时显示在页面上
+// 或者让 log 废弃（写一个空函数）
 // const log = function (s) {
 //     e('#id-text-log').value += '\n' + s
 // }
+
+const e = selector => document.querySelector(selector)
+
 const imageFromPath = function(path) {
     let img = new Image();
     img.src = path;
